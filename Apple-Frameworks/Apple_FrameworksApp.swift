bc9f98a53455f4a1956dedbf2c94413c2f14920f
Ashrf <ashrftvm1@gmail.com>
2025-09-06 23:06:49 +0400

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Asharaf Ali on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
